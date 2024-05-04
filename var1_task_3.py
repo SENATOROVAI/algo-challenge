b0fb66ea143694dc0dc6344568361fe6a8628bfb
vladlead5 <vlad.prog55@gmail.com>
2024-05-04 22:47:08 +0300

    Update var1_task_3.py

--- var1_task_3.py
+++ var1_task_3.py
@@ -18,12 +18,15 @@
 # Влад
 full_info = []
 with open("students.txt", encoding="utf-8") as info_students:
-    for student in info_students:
-        info = student.strip().split(",")
-        name = info[0].split()
+    # open file with information about students
+    for student in info_students:  # iterate over the lines of the file
+        info = student.strip().split(",")  # all information about students
+        name = info[0].split()  # last name, first name, patronymic
         full_info.append([name[0], name[1], name[2], info[1], info[2]])
-full_info.sort(key=lambda x: x[4])
-for person in full_info:
+        # add list element by element: last name, first name, patronymic,
+        # group, mark
+full_info.sort(key=lambda x: x[4])  # sort by marks
+for person in full_info:  # display information line by line
     print(person)
 
 # Realize quick search about a student by last name or patronymic.
