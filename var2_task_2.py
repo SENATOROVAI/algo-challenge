fc2a3cf57a90917f0d6e192c057aadc30b19a39b
Stepanov278 <164019359+Stepanov278@users.noreply.github.com>
2024-05-04 23:03:20 +0300

    Update var2_task_2.py
    Signed-off-by: Stepanov278 <164019359+Stepanov278@users.noreply.github.com>

--- var2_task_2.py
+++ var2_task_2.py
@@ -3,18 +3,20 @@
 их количество. Ограничений по использованию циклов и массивов нет.
 """
 
-def is_pandigital(num):
-    if len(str(num)) != 6:
-        return False
-    return len(set(str(num))) == 6
-
+def is_pandigital (num):
+# создание функции и приянтие числа
+if len(str(num)) != 6:
+return False
+return len (set (str (num) )) == 6
+# возвращает true если число 6 значное , false если наоборот
 count = 0
-for i in range(100000, 1000000):
-    if is_pandigital(i):
-        print(i)
-        count += 1
-
-print(count)
+for i in range (100000, 1000000):
+# перебор всех 6 ти значных цифр
+if is_pandigital (1):
+count += 1
+# счетчик count
+print (count )
+# вывод всех чисел удовлетворяющих условие
 
 #аналог через комбинаторику
 
