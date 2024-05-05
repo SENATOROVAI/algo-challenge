c7681a06b45bfc62d26e0cd33688e81e027f8d3a
Stepanov278 <164019359+Stepanov278@users.noreply.github.com>
2024-05-05 17:54:25 +0300

    Update var2_task_2.py
    Signed-off-by: Stepanov278 <164019359+Stepanov278@users.noreply.github.com>

--- var2_task_2.py
+++ var2_task_2.py
@@ -2,20 +2,31 @@
 Распечатать все шестизначные числа, в записи которых нет повторяющихся цифр, и вывести
 их количество. Ограничений по использованию циклов и массивов нет.
 """
+def is_pandigital(num):
+    if len(str(num)) != 6:
+        return False
+    return len(set(str(num))) == 6
 
-def is_pandigital (num):
-# создание функции и приянтие числа
-if len(str(num)) != 6:
-return False
-return len (set (str (num) )) == 6
-# возвращает true если число 6 значное , false если наоборот
 count = 0
-for i in range (100000, 1000000):
+ for i in range(100000, 1000000):
+    if is_pandigital(i):
+        count += 1
+
+print(count)
+
+#def is_pandigital (num):
+# создание функции и приянтие числа
+#if len(str(num)) != 6:
+#return False
+#return len (set (str (num) )) == 6
+# возвращает true если число 6 значное , false если наоборот
+#count = 0
+#for i in range (100000, 1000000):
 # перебор всех 6 ти значных цифр
-if is_pandigital (1):
-count += 1
+#if is_pandigital (1):
+#count += 1
 # счетчик count
-print (count )
+#print (count )
 # вывод всех чисел удовлетворяющих условие
 
 #аналог через комбинаторику
