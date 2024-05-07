5bd04aa8072b7e110454f1c5b6e6f5053789060a
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2024-05-07 20:43:10 +0300

    Update var1_task_4.py
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- var1_task_4.py
+++ var1_task_4.py
@@ -3,24 +3,20 @@
 находит максимальное из тех чисел, что встречаются в
  массиве три раза и более. Не использовать встроенные функции.
 """
-#Tatiana
-numbers_lst:list[int] = list(map(int, input().split())) # crate a list of numbers
-
-
 
 # Влад
 # O(N**2)
-# numbers = list(map(int, input().split()))
-# for start in range(len(numbers)):
-#     for cont in range(start + 1, len(numbers)):
-#         if numbers[start] > numbers[cont]:
-#             first_number = numbers[start]
-#             second_number = numbers[cont]
-#             numbers[cont], numbers[cont] = second_number, first_number
+numbers = list(map(int, input().split()))
+for start in range(len(numbers)):
+    for cont in range(start + 1, len(numbers)):
+        if numbers[start] > numbers[cont]:
+            first_number = numbers[start]
+            second_number = numbers[cont]
+            numbers[cont], numbers[cont] = second_number, first_number
 
-# maximum_search = [elem for elem in numbers if numbers.count(elem) >= 3]
-# try:
-#     print(maximum_search[-1])
-# except IndexError:
-#     print("Сheck the data is correct")
+maximum_search = [elem for elem in numbers if numbers.count(elem) >= 3]
+try:
+    print(maximum_search[-1])
+except IndexError:
+    print("Сheck the data is correct")
 # .\Влад
